09a32af14dad502041b37e0020f4b9cc36cef2f3
yujia21 <odonut@gmail.com>
2016-01-09 00:40:12 +0100

    update 09012016
    Changed position of carousel

--- content/content-welcome.php
+++ content/content-welcome.php
@@ -4,53 +4,56 @@
             <div class="jumbotron">
                 <div class="container">
                     <h1 style="text-align:center">Improve your language skills!</h1>
-                    <p class="text-center">Now supporting up to 7 languages!</p>
                 </div>
+                
                 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-        </ol>
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                </ol>
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-              <center><img src="images/carousel1.png"></center>
-              <div class="carousel-caption">Speak French</div>
-          </div>
-          <div class="item">
-              <center><img src="images/carousel2.png"></center>
-              <div class="carousel-caption">Speak Portuguese</div>
-          </div>
-          <div class="item">
-              <center><img src="images/carousel3.png"></center>
-              <div class="carousel-caption">Speak Spanish</div>
-          </div>
-          <div class="item">
-              <center><img src="images/carousel4.png"></center>
-              <div class="carousel-caption">Speak German</div>
-          </div>
-          <div class="item">
-              <center><img src="images/carousel5.png"></center>
-              <div class="carousel-caption">Speak Mandarin</div>
-          </div>
-        </div>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                  <div class="item active">
+                      <center><img src="images/carousel1.png"></center>
+                      <div class="carousel-caption">Speak French</div>
+                  </div>
+                  <div class="item">
+                      <center><img src="images/carousel2.png"></center>
+                      <div class="carousel-caption">Speak Portuguese</div>
+                  </div>
+                  <div class="item">
+                      <center><img src="images/carousel3.png"></center>
+                      <div class="carousel-caption">Speak Spanish</div>
+                  </div>
+                  <div class="item">
+                      <center><img src="images/carousel4.png"></center>
+                      <div class="carousel-caption">Speak German</div>
+                  </div>
+                  <div class="item">
+                      <center><img src="images/carousel5.png"></center>
+                      <div class="carousel-caption">Speak Mandarin</div>
+                  </div>
+                </div>
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+              <div class="container">
+                  <br>
+                  <p class="text-center">Now supporting up to 7 languages!</p>
+              </div>
             </div>
         </div>
     </header>
